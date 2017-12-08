6f07208115616f586c727194172eafcc2e44ecc6
romitamgai <rom_amgai@hotmail.com>
2017-12-08 06:38:40 +0545

    Fix tests

--- src/components/Home/Todo/TodoList.test.js
+++ src/components/Home/Todo/TodoList.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Todo from './Todo';
+import TodoList from './TodoList';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  render(<Todo />, div);
+  render(<TodoList todos={[]} />, div);
 });
